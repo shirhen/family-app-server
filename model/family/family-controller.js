627b0be275990b3ff351d88e065b943ef82d6f55
Shirhen Ince <shirhen23@gmail.com>
2016-11-19 19:27:14 +0200

    if list exists update is not insert

--- model/family/family-controller.js
+++ model/family/family-controller.js
@@ -3,12 +3,26 @@ const familyFacade  = require('./family-facade');
 
 class FamilyController extends Controller {
   createList(req, res, next) {
-    const conditions = { _id: req.params.id, 'lists.name': { $ne: req.body.name } };
+    const conditions = { _id: req.params.id, 'lists.name': { $eq: req.body.name } };
 
-    familyFacade.update(conditions, { $push: { lists:req.body } }, { upsert: true })
-    .then(doc => {
+    familyFacade.update(conditions, {
+      $set: {
+        'lists.$.name':req.body.name,
+        'lists.$.items':req.body.items
+      }
+    })
+    .then((doc) => {
       if (!doc) { return res.status(404).end(); }
-      return res.status(200).json(doc);
+      if (doc.nModified === 0) {
+        familyFacade.update({ _id: req.params.id }, {
+          $push: { lists:req.body }
+        }).then(doc => {
+          if (!doc) { return res.status(404).end(); }
+          return res.status(200).json({ doc });
+        }).catch(err => next(err));
+      } else {
+        return res.status(200).json({ doc });
+      }
     })
     .catch(err => next(err));
   }
