e4dbfab6179c4592ea211dce3ac74c1aad5a1759
Shirhen Ince <shirhen23@gmail.com>
2016-11-19 14:25:59 +0200

    update family lists when inserting a new list and valide the name is unique

--- model/family/family-controller.js
+++ model/family/family-controller.js
@@ -1,6 +1,17 @@
 const Controller = require('../../lib/controller');
 const familyFacade  = require('./family-facade');
 
-class FamilyController extends Controller {}
+class FamilyController extends Controller {
+  createList(req, res, next) {
+    const conditions = { _id: req.params.id, 'lists.name': { $ne: req.body.name } };
+
+    familyFacade.update(conditions, { $push: { lists:req.body } }, { upsert: true })
+    .then(doc => {
+      if (!doc) { return res.status(404).end(); }
+      return res.status(200).json(doc);
+    })
+    .catch(err => next(err));
+  }
+}
 
 module.exports = new FamilyController(familyFacade);
